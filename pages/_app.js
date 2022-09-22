f9dc26f98a662638b488920b5a20ddb8044fd2f7
Gunnar <44357421+sigurdarson@users.noreply.github.com>
2022-09-22 20:50:50 +0200

    fathom script update (#9)
    * fathom script update
    
    * updated fathom package

--- pages/_app.js
+++ pages/_app.js
@@ -1,49 +1,49 @@
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import Head from "next/head";
-import * as Fathom from "fathom-client";
-import { DefaultSeo } from "next-seo";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
+import React, { useState, useEffect } from 'react';
+import Router from 'next/router';
+import Head from 'next/head';
+import * as Fathom from 'fathom-client';
+import { DefaultSeo } from 'next-seo';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
-import SEO from "../next-seo.config";
+import SEO from '../next-seo.config';
 
 //Components
-import Header from "../components/header/header";
+import Header from '../components/header/header';
 
 //Themes
 const lightTheme = {
-  body: "#ffffff",
-  titleColor: "#141414",
-  bodyText: "#14141482",
-  panel: "#fafafa",
-  panelHover: "#14141412",
-  border: "#14141406",
-  toggle: "#7868D5",
-  tag: "#14141412",
-  navigator: "rgba(255, 255, 255, 0.82)",
-  navigatorBorder: "#14141412",
-  shadow: "rgb(255, 255, 255, 1)",
+  body: '#ffffff',
+  titleColor: '#141414',
+  bodyText: '#14141482',
+  panel: '#fafafa',
+  panelHover: '#14141412',
+  border: '#14141406',
+  toggle: '#7868D5',
+  tag: '#14141412',
+  navigator: 'rgba(255, 255, 255, 0.82)',
+  navigatorBorder: '#14141412',
+  shadow: 'rgb(255, 255, 255, 1)',
 
   colors: {
-    red: "#EC506E",
+    red: '#EC506E',
   },
 };
 
 const darkTheme = {
-  body: "#000000",
-  titleColor: "#ffffff",
-  bodyText: "#ffffff82",
-  panel: "#141414",
-  panelHover: "#ffffff18",
-  border: "#ffffff06",
-  toggle: "#F5A623",
-  tag: "#ffffff12",
-  navigator: "rgba(20, 20, 20, 0.82)",
-  navigatorBorder: "#ffffff12",
-  shadow: "rgb(0, 0, 0, 1)",
+  body: '#000000',
+  titleColor: '#ffffff',
+  bodyText: '#ffffff82',
+  panel: '#141414',
+  panelHover: '#ffffff18',
+  border: '#ffffff06',
+  toggle: '#F5A623',
+  tag: '#ffffff12',
+  navigator: 'rgba(20, 20, 20, 0.82)',
+  navigatorBorder: '#ffffff12',
+  shadow: 'rgb(0, 0, 0, 1)',
 
   colors: {
-    red: "#EC506E",
+    red: '#EC506E',
   },
 };
 
@@ -83,59 +83,48 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+// Record a pageview when route changes
+Router.events.on('routeChangeComplete', (as, routeProps) => {
+  if (!routeProps.shallow) {
+    Fathom.trackPageview();
+  }
+});
+
 export default function App({ Component, pageProps }) {
-  //Page loader
-  const router = useRouter();
-
+  // Initialize Fathom when the app loads
   useEffect(() => {
-    // Initialize Fathom when the app loads
-    // Example: yourdomain.com
-    //  - Do not include https://
-    //  - This must be an exact match of your domain.
-    //  - If you're using www. for your domain, make sure you include that here.
-    Fathom.load("ALSMHWAO", {
-      includedDomains: ["myprivacy.is"],
-      url: "https://moth.myprivacy.is/script.js",
+    Fathom.load('ALSMHWAO', {
+      includedDomains: ['myprivacy.is'],
+      url: 'https://efficient-happy.myprivacy.is/script.js',
     });
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-    // Record a pageview when route changes
-    router.events.on("routeChangeComplete", onRouteChangeComplete);
-
-    // Unassign event listener
-    return () => {
-      router.events.off("routeChangeComplete", onRouteChangeComplete);
-    };
   }, []);
 
-  const [theme, setTheme] = useState("dark");
-  const isDarkTheme = theme === "dark";
+  const [theme, setTheme] = useState('dark');
+  const isDarkTheme = theme === 'dark';
   const themeToggler = () => {
-    const updatedTheme = isDarkTheme ? "light" : "dark";
+    const updatedTheme = isDarkTheme ? 'light' : 'dark';
     setTheme(updatedTheme);
-    localStorage.setItem("theme", updatedTheme);
+    localStorage.setItem('theme', updatedTheme);
   };
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
+    const savedTheme = localStorage.getItem('theme');
     const prefersDark =
       window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
-    if (savedTheme && ["dark", "light"].includes(savedTheme)) {
+      window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (savedTheme && ['dark', 'light'].includes(savedTheme)) {
       setTheme(savedTheme);
     } else if (prefersDark) {
-      setTheme("dark");
+      setTheme('dark');
     }
   }, []);
 
   return (
     <>
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/img/meta/favicon.svg" />
-        <link rel="icon" type="image/png" href="/img/meta/favicon.png" />
-        <meta name="robots" content="follow, index" />
+        <link rel='icon' type='image/svg+xml' href='/img/meta/favicon.svg' />
+        <link rel='icon' type='image/png' href='/img/meta/favicon.png' />
+        <meta name='robots' content='follow, index' />
       </Head>
       <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
         <GlobalStyle />
